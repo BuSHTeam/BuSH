e5d2b7223ffefbcbc565ae7089d502dae3b2daf4
Anderson Almada <andersonalmadagomes@yahoo.com.br>
2015-04-23 18:39:33 -0300

    Implementado a API de reconhecimento (andando, no veiculo, ..) do android

--- app/src/main/java/br/ufc/bush/MainActivity.java
+++ app/src/main/java/br/ufc/bush/MainActivity.java
@@ -1,17 +1,27 @@
 package br.ufc.bush;
 
+import android.app.PendingIntent;
 import android.app.ProgressDialog;
-import android.graphics.drawable.AnimationDrawable;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.ActivityRecognition;
+import com.google.android.gms.location.LocationServices;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
@@ -21,17 +31,23 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends ActionBarActivity {
+import br.ufc.utils.ActivityRecognitionIntentService;
+
+public class MainActivity extends ActionBarActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
     private static final String HOST = "http://www.sefiro.com.br/";
     private static final String FILE = "bush/location.php";
     private static final String TAG = "BuSH";
+
+    private Context mContext;
+    private GoogleApiClient mGApiClient;
+    private BroadcastReceiver receiver;
+    private TextView tvName;
+
     String  id = "";
 
     @Override
@@ -39,7 +55,54 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tvName = (TextView) findViewById(R.id.tvName);
+
+        //Set the context
+        mContext = this;
+
+        //Check Google Play Service Available
+        if(isPlayServiceAvailable()) {
+            mGApiClient = new GoogleApiClient.Builder(this)
+                    .addApi(LocationServices.API)
+                    .addApi(ActivityRecognition.API)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .build();
+            //Connect to gPlay
+            mGApiClient.connect();
+        }else{
+            Log.d(TAG, "Google Play Service not Available");
+        }
+
+        //Broadcast receiver
+        receiver  = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String v =  "Activity :" +
+                        intent.getStringExtra("act") + " " +
+                        "Confidence : " + intent.getExtras().getInt("confidence") + "n";
+
+                v += tvName.getText();
+                tvName.setText(v);
+            }
+        };
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("SAVVY");
+        registerReceiver(receiver, filter);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
         new RegisterTask().execute(HOST+FILE, "anderson");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mGApiClient.disconnect();
+        unregisterReceiver(receiver);
     }
 
     @Override
@@ -64,11 +127,35 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onConnected(Bundle bundle) {
+        Intent i = new Intent(this, ActivityRecognitionIntentService.class);
+        PendingIntent mActivityRecongPendingIntent = PendingIntent.getService(this, 0, i, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Log.d(TAG, "onConnected: " + mActivityRecongPendingIntent.toString() );
+        ActivityRecognition.ActivityRecognitionApi.requestActivityUpdates(mGApiClient, 0, mActivityRecongPendingIntent);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d(TAG, "onConnectionSuspended");
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.d(TAG, "onConnectionFailed");
+    }
+
+    private boolean isPlayServiceAvailable() {
+        return GooglePlayServicesUtil.isGooglePlayServicesAvailable(mContext) == ConnectionResult.SUCCESS;
+    }
+
+    // Classe para conexao Android_PHP
     private class RegisterTask extends AsyncTask<String, Void, Boolean> {
         private final ProgressDialog dialog = new ProgressDialog(MainActivity.this);
 
         protected void onPreExecute() {
-            dialog.setMessage("Signing in...");
+            dialog.setMessage("Loading ...");
             dialog.show();
         }
 
