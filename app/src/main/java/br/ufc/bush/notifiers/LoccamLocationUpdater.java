393340946eeed52553ac12de1db5375e06268532
FelipeBarreto <great@IOT-BARRETO.LGE.NET>
2015-05-14 16:40:02 -0300

    refactoring the map

---
+++ app/src/main/java/br/ufc/bush/notifiers/LoccamLocationUpdater.java
@@ -0,0 +1,33 @@
+package br.ufc.bush.notifiers;
+
+import com.google.android.gms.maps.model.LatLng;
+
+/**
+ * Created by great on 05/05/2015.
+ */
+public class LoccamLocationUpdater extends LocationUpdater {
+
+    private static LocationUpdater instance;
+
+    public static LocationUpdater getInstance() {
+        if(instance == null){
+            instance = new DirectLocationUpdater();
+        }
+        return instance;
+    }
+
+    @Override
+    public void startLocationService() {
+
+    }
+
+    @Override
+    public void stopLocationService() {
+
+    }
+
+    @Override
+    public LatLng getLocation() {
+        return null;
+    }
+}
